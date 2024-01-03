a7f80c25f4a9b15043248dc09c577d751ec55eab
Andrew Howden <hello@andrewhowden.com>
2024-01-03 10:43:57 +0100

    Point x40.dev to GitHub pages
    This commit points x40.dev to GitHub pages, which will be the hosting
    platform for the documentation for this project.
    
    == Design Notes
    === ALIAS record
    
    Within this work, I discovered that there is a (relatively new?) type of
    record called "ALIAS", which functions as a "CNAME" record but for the
    apex domain.
    
    === GitHub Pages
    
    GitHub pages was chosen as while doing work to build the GitHub runner,
    I discovered a task which publishes mkdocs based information to GitHub
    pages. This is the ideal workflow, and low overhead.

--- deploy/prod/tf/dns.tf
+++ deploy/prod/tf/dns.tf
@@ -1,4 +1,4 @@
-// The DNS file contains all information related to maintaining DNS zones, and the records within the zones for this 
+// The DNS file contains all information related to maintaining DNS zones, and the records within the zones for this
 // project.
 resource "google_dns_managed_zone" "x40-link" {
     name = "x40-link"
@@ -12,3 +12,21 @@ resource "google_dns_managed_zone" "x40-dev" {
     description = "The developer documentation for the x40 project."
 }
 
+resource "google_dns_record_set" "x40-dev__ALIAS" {
+    managed_zone = google_dns_managed_zone.x40-dev.name
+    name = google_dns_managed_zone.x40-dev.dns_name
+    ttl = 300
+    type = "ALIAS"
+
+    rrdatas = ["andrewhowdencom.github.io."]
+}
+
+resource "google_dns_record_set" "x40-dev__CNAME" {
+    managed_zone = google_dns_managed_zone.x40-dev.name
+    name = "www.${google_dns_managed_zone.x40-dev.dns_name}"
+    ttl = 300
+    type = "CNAME"
+
+    rrdatas = ["andrewhowdencom.github.io."]
+
+}
