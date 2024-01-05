a440dba641ecd4325de6c27cbbd5527b9e267b24
Andrew Howden <hello@andrewhowden.com>
2024-01-05 15:14:04 +0000

    Issue certificates for h4n.me (#18)
    Currently, the certificates generated by the application are only valid
    for x40.link. With the personal use of h4n.me, this means those links
    are unusable. This commit addresses that by making these links usable
    again

--- deploy/prod/tf/vars.tf
+++ deploy/prod/tf/vars.tf
@@ -3,6 +3,11 @@ variable "x40_link_domains" {
   default = ["x40.link", "www.x40.link", "app.x40.link", "api.x40.link"]
 }
 
+variable "h4n_me_domains" {
+    type = list(string)
+    default = ["h4n.me", "www.h4n.me"]
+}
+
 variable "cloud-run__x40-link" {
   type    = string
   default = "x40-link"
