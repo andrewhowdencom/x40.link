f814dfb5849602e4e4a5a446e5c3defea02a46a5
Andrew Howden <hello@andrewhowden.com>
2024-01-22 16:01:24 +0100

    Implement token generation (via "Seed")
    Currently, the broad work stream is to create authentication that can be
    used between the client and the server. Part of that work is generating
    tokens that the client can send to the server, and then authorize them.
    
    Without going into detail, the plan for this work is to implement a
    "TokenSource", and have that "TokenSource" be cache aware. This means it
    reads its initial state from disk, and if the disk is empty, needs the
    cache to be "seeded".
    
    This commit implements the seed.
    
    == Design Notes
    === No OpenID
    
    This commit is notable as while it generates tokens, it does not
    generate tokens that are OpenID compatible. This is because apparently,
    OpenID tokens are designed to be used on the client, and authentication
    tokens used on the server¹
    
    Auth0 provides support for JWT access tokens, and the existing
    enrichment provides the roles.
    
    —
    1. https://auth0.com/docs/secure/tokens/access-tokens
    2. https://www.oauth.com/oauth2-servers/openid-connect/id-tokens/
    
    === Auth Flow: Device
    
    The chosen authenticatication flow is the one for non-interactive
    devices; namely, the device flow. This is because this flow does not
    require a secret key to be persisted in the application; rather, it
    generates a "token" that is single use and for the device session. The
    user then authenticates on another device.
    
    === HTTP Testing
    
    This commit also goes some way to figuring out how to test OAuth by
    creating a HTTP Transport RoundTripper that simply uses a chi
    multiplexer to handle requests. This is very efficient, though the
    requests still take a second as the ticker can only be reduced down to a
    second.

---
+++ api/auth/tokens/seeds/seeds.go
@@ -0,0 +1,46 @@
+// Package seeds provides a way to "seed" a token source; especially one that relies on caching. This allows using
+// the "seed" to bootstrap authentication in the case the cache is empty (or new), but doesn't call it every time.
+package seeds
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"golang.org/x/oauth2"
+)
+
+// Err* are sentinel errors
+var (
+	ErrFailedToExchange     = errors.New("failed to exchange token")
+	ErrFailedToGetDeviceURL = errors.New("failed to get device url")
+)
+
+// Seed is a function that handles all of the initial authentication, so that we have a token (complete with refresh).
+type Seed func(context.Context) (*oauth2.Token, error)
+
+// DeviceAuth returns a seed that creates the initial OAuth2 flow via the Device flow. See:
+// https://oauth.net/2/device-flow/
+func DeviceAuth(audience string, c *oauth2.Config) Seed {
+	return func(ctx context.Context) (*oauth2.Token, error) {
+		// Initialize the token source with the full flow.
+		response, err := c.DeviceAuth(ctx, oauth2.SetAuthURLParam("audience", audience))
+		if err != nil {
+			return nil, fmt.Errorf("%w: %s", ErrFailedToGetDeviceURL, err)
+		}
+
+		fmt.Printf(
+			"Please enter the code %s at %s\n",
+			response.UserCode,
+			response.VerificationURI,
+		)
+
+		tok, err := c.DeviceAccessToken(ctx, response, oauth2.AccessTypeOffline)
+
+		if err != nil {
+			return nil, fmt.Errorf("%w: %s", ErrFailedToExchange, err)
+		}
+
+		return tok, nil
+	}
+}
---
+++ api/auth/tokens/seeds/seeds_test.go
@@ -0,0 +1,163 @@
+package seeds_test
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/andrewhowdencom/x40.link/api/auth/tokens/seeds"
+	"github.com/go-chi/chi/v5"
+	"github.com/stretchr/testify/assert"
+	"golang.org/x/oauth2"
+)
+
+type roundTripper struct {
+	h http.Handler
+}
+
+func (rt *roundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
+	w := httptest.NewRecorder()
+	rt.h.ServeHTTP(w, req)
+	return w.Result(), nil
+}
+
+func TestSeedDeviceAuth(t *testing.T) {
+	for _, tc := range []struct {
+		name string
+
+		client *http.Client
+
+		tok *oauth2.Token
+		err error
+	}{
+		{
+			name: "failed to generate code",
+			client: &http.Client{
+				Transport: &roundTripper{
+					h: func() http.Handler {
+						mux := chi.NewMux()
+
+						mux.Post("/oauth/device/code", func(w http.ResponseWriter, r *http.Request) {
+							w.WriteHeader(http.StatusInternalServerError)
+						})
+
+						return mux
+					}(),
+				},
+			},
+			tok: nil,
+			err: seeds.ErrFailedToGetDeviceURL,
+		},
+		{
+			name: "failed to exchange token",
+			client: &http.Client{
+				Transport: &roundTripper{
+					h: func() http.Handler {
+						mux := chi.NewMux()
+
+						mux.Post("/oauth/device/code", func(w http.ResponseWriter, r *http.Request) {
+							auth := &oauth2.DeviceAuthResponse{
+								DeviceCode:              "TEST-DEVICE-CODE",
+								UserCode:                "TEST-USER-CODE",
+								VerificationURI:         "https://x40.local/activate",
+								VerificationURIComplete: "https://x40.local/activate?code=TEST-USER-CODE",
+								Expiry:                  time.Now().Add(time.Hour),
+								Interval:                1,
+							}
+
+							m := json.NewEncoder(w)
+							if err := m.Encode(auth); err != nil {
+								panic("could not write to body: " + err.Error())
+							}
+						})
+
+						mux.Post("/oauth/token", func(w http.ResponseWriter, r *http.Request) {
+							w.WriteHeader(http.StatusInternalServerError)
+						})
+
+						return mux
+					}(),
+				},
+			},
+			tok: nil,
+			err: seeds.ErrFailedToExchange,
+		},
+		{
+			name: "all ok",
+			client: &http.Client{
+				Transport: &roundTripper{
+					h: func() http.Handler {
+						mux := chi.NewMux()
+
+						mux.Post("/oauth/device/code", func(w http.ResponseWriter, r *http.Request) {
+							auth := &oauth2.DeviceAuthResponse{
+								DeviceCode:              "TEST-DEVICE-CODE",
+								UserCode:                "TEST-USER-CODE",
+								VerificationURI:         "https://x40.local/activate",
+								VerificationURIComplete: "https://x40.local/activate?code=TEST-USER-CODE",
+								Expiry:                  time.Now().Add(time.Hour),
+								Interval:                1,
+							}
+
+							m := json.NewEncoder(w)
+							if err := m.Encode(auth); err != nil {
+								panic("could not write to body: " + err.Error())
+							}
+						})
+
+						mux.Post("/oauth/token", func(w http.ResponseWriter, r *http.Request) {
+							w.Header().Set("Content-Type", "application/json")
+
+							tok := &oauth2.Token{
+								AccessToken:  "TEST-ACCESS-TOKEN",
+								RefreshToken: "TEST-REFRESH-TOKEN",
+								Expiry:       time.Now().Add(time.Hour),
+								TokenType:    "Bearer",
+							}
+
+							m := json.NewEncoder(w)
+							if err := m.Encode(tok); err != nil {
+								panic("could not write to body " + err.Error())
+							}
+						})
+
+						return mux
+					}(),
+				},
+			},
+			tok: &oauth2.Token{
+				AccessToken: "TEST-ACCESS-TOKEN",
+			},
+		},
+	} {
+		tc := tc
+
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			ctx := context.WithValue(context.Background(), oauth2.HTTPClient, tc.client)
+
+			seed := seeds.DeviceAuth("https://www.example.local", &oauth2.Config{
+				Endpoint: oauth2.Endpoint{
+					DeviceAuthURL: "https://x40.local/oauth/device/code",
+					TokenURL:      "https://x40.local/oauth/token",
+				},
+				ClientID: "TEST-CLIENT-ID",
+			})
+
+			tok, err := seed(ctx)
+
+			assert.ErrorIs(t, err, tc.err)
+
+			if tc.tok == nil {
+				assert.Nil(t, tok)
+			} else {
+				// Here, we're just validating that we get back *a* token. The rest of it is validated by the oAuth library itself.
+				assert.Equal(t, tc.tok.AccessToken, tok.AccessToken)
+			}
+		})
+	}
+
+}
